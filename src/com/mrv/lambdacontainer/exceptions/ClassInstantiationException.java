d7421a9446c82ed88a2b178c84b84dcd11e73930
Marcello Valle <marcellorvalle@gmail.com>
2016-02-10 17:05:32 -0200

    Added documentation

--- src/com/mrv/lambdacontainer/exceptions/ClassInstantiationException.java
+++ src/com/mrv/lambdacontainer/exceptions/ClassInstantiationException.java
@@ -4,6 +4,10 @@ package com.mrv.lambdacontainer.exceptions;
  * Exception used when a class can not be instantiated.
  */
 public class ClassInstantiationException extends Exception {
+    /**
+     * Simple invocation with a message.
+     * @param message
+     */
     public ClassInstantiationException(String message) {
         super(message);
     }
